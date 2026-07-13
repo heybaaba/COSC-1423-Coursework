57b69f8b429b3f33a46726dac9acf989f79a4a60
Baaba Nhyira Baidoo <165162223+heybaaba@users.noreply.github.com>
2026-07-12 22:11:06 -0500

    Add files via upload

---
+++ odd_substitutions.py
@@ -0,0 +1,25 @@
+# Baaba Nhyira Baidoo
+# Time Taken: 2 hours 45 minutes
+# AI prompt: I asked AI to explain the assignment
+# Comment: This project helped me practice inputs and printing sentences. Also helped me figure out what I did wrong during the lab section.
+
+# The Itsy Bitsy Spider - Odd Substitutions
+
+print("Another exciting game of Odd Substitutions!")
+print()
+
+#Ask the user for six words to use in the rhyme
+adjective = input("Adjective: ")
+animal = input("Animal: ")
+thing = input("Thing: ")
+precipitation = input("Precipitation: ")
+verb1 = input("Verb (past tense): ")
+verb2 = input("Verb (past tense):")
+print()
+
+# Use the user's words to print a new version of "The Itsy Bitsy Spider"
+print(f"The {adjective} {animal} went up the {thing}")
+print(f"Down came the {precipitation} and {verb1} the {animal} out")
+print(f"Out came the sun and {verb2} all the {precipitation}")
+print(f"The {adjective} {animal} went up the {thing} again")
+print()
